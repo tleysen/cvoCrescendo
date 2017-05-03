9eeb78607fd805d73cbf8189144ea00f14ef81b2
Liamdelee <liam.delee@student.ap.be>
2017-05-03 16:42:44 +0200

    V0.093 adding navbar functionalitie

--- CVOCrescendo/src/main/resources/static/js/hello.js
+++ CVOCrescendo/src/main/resources/static/js/hello.js
@@ -1,9 +1,48 @@
-angular.module('informat', [])
-    .controller('home', function($scope, $http) {
-        $http.get('/courses').success(function(data) {
-            $scope.courses = data;
+// script.js
+
+// create the module and name it informat
+// also include ngRoute for all our routing needs
+var informat = angular.module('informat', ['ngRoute']);
+
+// configure our routes
+informat.config(function($routeProvider) {
+    $routeProvider
+
+    // route for the home page
+        .when('/', {
+            templateUrl : 'pages/home.html',
+            controller  : 'mainController'
         })
-        $http.get('/students').success(function(data) {
-            $scope.students = data;
+
+        // route for the about page
+        .when('/about', {
+            templateUrl : 'pages/about.html',
+            controller  : 'aboutController'
         })
-    });
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
+        });
+});
+
+// create the controller and inject Angular's $scope
+informat.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+informat.controller('aboutController', function($scope, $http) {
+    $http.get('/courses').success(function(data) {
+        $scope.courses = data;
+    })
+    $http.get('/students').success(function(data) {
+        $scope.students = data;
+    })
+});
+
+informat.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
+
