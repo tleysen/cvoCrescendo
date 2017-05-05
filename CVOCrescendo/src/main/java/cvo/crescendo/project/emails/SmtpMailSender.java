fe8dbdafb5860a1649093560d6804384a09a64dd
MaartenExelmans <s090083@ap.be>
2017-05-05 10:56:30 +0200

    adding code for auto mail

---
+++ CVOCrescendo/src/main/java/cvo/crescendo/project/emails/SmtpMailSender.java
@@ -0,0 +1,39 @@
+package cvo.crescendo.project.*;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SmtpMailSender {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	public void send(String[] to, String subject, String body) throws MessagingException {
+
+		MimeMessage message = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper;
+
+
+		helper = new MimeMessageHelper(message, true); // true indicates
+		// multipart message
+		helper.setFrom("xxxxxxx@xxx.com"); // change to smartschool Email
+		helper.setSubject(subject);
+		helper.setTo(to);
+		helper.setText(body, true); // true indicates html
+		// continue using helper object for more functionalities like adding attachments, etc.
+
+		javaMailSender.send(message);
+
+
+	}
+
+}
